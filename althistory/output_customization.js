0ec71bb9385ffd6c5ab33902364fff8a1e35ba6b
smeeren <boopalaarul@yahoo.com>
2020-12-06 21:24:45 -0800

    revisions to copyimg, take two

--- althistory/output_customization.js
+++ althistory/output_customization.js
@@ -21,13 +21,16 @@ function fontOut() {
 //screenshot of output
 function copyImg() {
 	//trad. JS to select an element, and to append it to DOM
+	alert("A")
 	var old_canvas = document.getElementById("canvas");
 	if (old_canvas !== null) {
 		old_canvas.remove();
 	}
+	alert("B")
 	html2canvas(document.querySelector("#output")).then(canvas => {
 		document.querySelector("#output").parentNode.insertBefore(canvas, document.querySelector("#output").nextSibling);
 		canvas.id = "canvas"
 	});
+	alert("C")
 	//saveAsPNG(canvas, canvas.width, canvas.height);
 }
